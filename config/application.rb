ba2b00c719a50e716f009c794213cb040bc6d2e3
Tasos Laskos <tasos.laskos@gmail.com>
2012-08-10 05:24:04 +0300

    rails_apps_composer: authentication and authorization

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module ArachniWebui
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -7,3 +7,7 @@ require 'machinist/active_record'
 #     title { "Post #{sn}" }
 #     body  { "Lorem ipsum..." }
 #   end
+
+User.blueprint do
+  # Attributes here
+end
