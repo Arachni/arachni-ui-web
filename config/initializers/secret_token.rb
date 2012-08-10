691018ba391d7e7e847d09900c83d0c93dd88ba9
Tasos Laskos <tasos.laskos@gmail.com>
2012-08-10 05:08:33 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArachniWebui::Application.config.secret_token = 'f87edd83cb3486098cf0f79759a5ca267dfe65ae7a7bea42dd7a6a399fc9582ac3fde818b498eab8d15530f8e68c501b403dee6fac9fbf04e9e8cf4859f6793b'
