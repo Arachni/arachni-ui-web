cfbab70018ffc10d16020202dcebc35702a0a95b
Tasos Laskos <tasos.laskos@gmail.com>
2012-08-10 05:23:58 +0300

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ end
 
 module ArachniWebui
   class Application < Rails::Application
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+      g.fixture_replacement :machinist
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
