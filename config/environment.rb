69511d74779d315344b7076eda64f76f9f9e2446
Tasos Laskos <tasos.laskos@gmail.com>
2012-12-19 03:24:42 +0200

    set default internal and external encoding to UTF8

--- config/environment.rb
+++ config/environment.rb
@@ -14,6 +14,9 @@
     limitations under the License.
 =end
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Load the rails application
 require File.expand_path( '../application', __FILE__ )
 
