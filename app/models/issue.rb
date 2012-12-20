51ec4549d07a3a79e239a5e610a1506f8142d715
Tasos Laskos <tasos.laskos@gmail.com>
2012-12-20 23:31:57 +0200

    Issues restricted to a unique digest per scan

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,8 @@
 class Issue < ActiveRecord::Base
     belongs_to :scan
 
+    validates_uniqueness_of :digest, scope: :scan_id
+
     attr_accessible :cvssv2, :cwe, :description, :elem, :method, :name,
                     :references, :remedy_code, :remedy_guidance, :scan_id,
                     :severity, :url, :verification, :digest, :var
