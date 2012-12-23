86dbd544ee10e02909635685725ab6d0c83b5d7e
Tasos Laskos <tasos.laskos@gmail.com>
2012-12-23 23:34:34 +0200

    tiny formatting update

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -205,7 +205,8 @@ class Scan < ActiveRecord::Base
 
         instance.service.
             progress( with: [:native_issues, errors: error_messages.to_s.lines.count],
-                      without: [ issues: [ issue_digests ] ] ) do |progress_data|
+                      without: [ issues: issue_digests ] ) do |progress_data|
+
             if progress_data.rpc_exception?
                 self.status = :error
                 save
