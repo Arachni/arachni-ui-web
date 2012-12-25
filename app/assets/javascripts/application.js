6d9c7ad35083c5efe842b9b44cfe203a5c9b7d23
Tasos Laskos <tasos.laskos@gmail.com>
2012-12-25 20:57:31 +0200

    added ; to EOLs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -86,26 +86,26 @@ $(window).ready( function( $ ) {
     var $win = $(window),
         $nav = $('.subnav' ),
         navTop = $('header').height() - $nav.height(),
-        isFixed = 0
+        isFixed = 0;
 
-    processScroll()
+    processScroll();
 
     // hack sad times - holdover until rewrite for 2.1
     $nav.on( 'click', function () {
         if( !isFixed ) setTimeout( function () { $win.scrollTop($win.scrollTop() - 47) }, 10 );
-    })
+    });
 
-    $win.on( 'scroll', processScroll )
+    $win.on( 'scroll', processScroll );
 
     function processScroll() {
-        var i, scrollTop = $win.scrollTop()
+        var i, scrollTop = $win.scrollTop();
         if( scrollTop >= navTop && !isFixed ) {
-            isFixed = 1
-            $nav.addClass( 'subnav-fixed' )
+            isFixed = 1;
+            $nav.addClass( 'subnav-fixed' );
             $nav.css( 'top', $('header').height() );
         } else if( scrollTop <= navTop && isFixed ) {
-            isFixed = 0
-            $nav.removeClass( 'subnav-fixed' )
+            isFixed = 0;
+            $nav.removeClass( 'subnav-fixed' );
         }
     }
-})
+});
