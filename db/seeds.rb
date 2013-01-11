8568d8faf17b4406dd889dd1c9b5e243c2de2b95
Tasos Laskos <tasos.laskos@gmail.com>
2013-01-11 22:16:16 +0200

    Updated seeds:   * Only an admin user now created   * Profiles use default values from Arachni::Options

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,51 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 puts 'SETTING UP DEFAULT USERS'
-user = User.create! name: 'First User', email: 'user@example.com',
-                    password: 'please', password_confirmation: 'please'
+user = User.create! name:                  'Administrator',
+                    email:                 'admin@admin.com',
+                    password:              'administrator',
+                    password_confirmation: 'administrator'
 user.add_role :admin
-puts 'New user created: ' << user.name
+puts 'Admin user created: ' << user.name
 
-user2 = User.create! name: 'Second User', email: 'user2@example.com',
-                     password: 'please', password_confirmation: 'please'
-puts 'New user created: ' << user2.name
+arachni_defaults = {}
+
+ignore = %w(max_slaves)
+profile_columns = Profile.column_names
+Arachni::Options.to_h.each do |k, v|
+    next if v.nil? || !profile_columns.include?( k ) || ignore.include?( k )
+
+    arachni_defaults[k.to_sym] = v
+end
+
+arachni_defaults.merge!(
+    name:          'Default',
+    description:   'Sensible, default settings.',
+    audit_links:   true,
+    audit_forms:   true,
+    audit_cookies: true,
+    plugins:       :default
+)
 
 puts 'SETTING UP DEFAULT PROFILE'
-p = Profile.create! name: 'Default', audit_links: true, audit_forms: true,
-                audit_cookies: true, http_req_limit: 20, modules: :all,
-                plugins: :default, description: 'Relatively sensible configuration settings.'
+p = Profile.create! arachni_defaults.merge(
+                        name:          'Default',
+                        description:   'Sensible, default settings.',
+                        modules:       :all
+                    )
 p.make_default
 puts 'Default profile created: ' << p.name
 
-p = Profile.create! name: 'XSS', audit_links: true, audit_forms: true,
-                    audit_cookies: true, http_req_limit: 20,
-                    modules: %w(xss xss_path xss_tag xss_script_tag xss_event),
-                    plugins: :default, description: 'Scans for XSS vulnerabilities.'
+p = Profile.create! arachni_defaults.merge(
+                        name: 'Cross-Site Scripting (XSS)',
+                        description: 'Scans for Cross-Site Scripting (XSS) vulnerabilities.',
+                        modules: %w(xss xss_path xss_tag xss_script_tag xss_event)
+                    )
 puts 'XSS profile created: ' << p.name
 
-p = Profile.create! name: 'XSS - forms', audit_forms: true,
-                    http_req_limit: 20,
-                    modules: %w(xss xss_path xss_tag xss_script_tag xss_event),
-                    plugins: :default, description: 'Scans for XSS vulnerabilities.'
-puts 'XSS profile created: ' << p.name
+p = Profile.create! arachni_defaults.merge(
+                        name: 'SQL injection',
+                        description: 'Scans for SQL injection vulnerabilities.',
+                        modules: %w(sqli sqli_blind_rdiff sqli_blind_timing)
+                    )
+puts 'SQLi profile created: ' << p.name
