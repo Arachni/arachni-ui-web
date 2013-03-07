e591f4f92283c133a80302c55fdb753ed268f9e3
Tasos Laskos <tasos.laskos@gmail.com>
2013-03-07 05:44:22 +0200

    Profile: Plugin options can now have custom handlers

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -19,6 +19,22 @@ module ProfilesHelper
         render partial: 'attribute_messages', locals: { attribute: attribute }
     end
 
+    def render_options( plugin_name, plugin_info, configuration = {}, disabled = false )
+        tpl = "#{Rails.root}/app/views/profiles/plugin_options/#{plugin_name}/#{params[:action]}.html.erb"
+        opts = {
+            plugin_name:   plugin_name,
+            info:          plugin_info,
+            configuration: configuration,
+            disabled:      disabled
+        }
+
+        if File.exist? tpl
+            render file: tpl, locals: opts
+        else
+            render partial: 'profiles/plugin_options/generic', locals: opts
+        end
+    end
+
     def profile_filter( filter )
         filter ||= 'yours'
 
