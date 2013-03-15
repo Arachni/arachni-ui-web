d10cab8c15616dd4de27236cd6315bf0c26eef04
Tasos Laskos <tasos.laskos@gmail.com>
2013-03-15 02:27:47 +0200

    Added capybara requires to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require File.expand_path( "../../config/environment", __FILE__ )
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require 'capybara/rspec'
+require 'capybara/rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join( "spec/support/**/*.rb" )].each { |f| require f }
