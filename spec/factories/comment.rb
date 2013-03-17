25f9dfbce201477268a62bd44c09fecb49ff2101
Tasos Laskos <tasos.laskos@gmail.com>
2013-03-17 22:43:12 +0200

    Comment: Added model test

---
+++ spec/factories/comment.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+    factory :comment do
+        text { Faker::Lorem.paragraph }
+        association :commentable, factory: :issue
+    end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'should have a valid factory' do
+        FactoryGirl.create(:comment).should be_valid
+    end
+
+    it 'is invalid without :text String' do
+        FactoryGirl.build( :comment, text: nil ).should be_invalid
+    end
+
+    it 'is invalid with empty :text String' do
+        FactoryGirl.build( :comment, text: '' ).should be_invalid
+    end
+
+    describe '#text' do
+        it 'returns the comment text' do
+            text = Faker::Lorem.paragraph
+            comment = FactoryGirl.create( :comment, text: text )
+
+            comment.should be_valid
+            comment.text.should == text
+        end
+    end
+
+    describe '#commentable' do
+        it 'returns the parent object' do
+            comment = FactoryGirl.create( :comment )
+            comment.commentable.should be_a_kind_of( Issue )
+        end
+    end
+
 end
