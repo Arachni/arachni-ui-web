fe147529e3e461c01149470c1547a845665686cb
Tasos Laskos <tasos.laskos@gmail.com>
2013-03-22 20:02:58 +0200

    Replaced existing users.rb factory with user.rb which uses Faker.

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+    factory :user do |d|
+        name { Faker::Name.name }
+        email { Faker::Internet.email }
+        password { 'pass123456' }
+        password_confirmation { 'pass123456' }
+    end
+
+end
