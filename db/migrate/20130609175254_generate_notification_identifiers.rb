14ccba8009bd0cc1de1a56b3c0f2876a02f63462
Tasos Laskos <tasos.laskos@gmail.com>
2013-06-21 20:52:38 +0300

    db/migrate: notification => Notification

--- db/migrate/20130609175254_generate_notification_identifiers.rb
+++ db/migrate/20130609175254_generate_notification_identifiers.rb
@@ -1,7 +1,7 @@
 class GenerateNotificationIdentifiers < ActiveRecord::Migration
   def self.up
       Notification.all.find_each do |notification|
-          notification.update_all( { identifier: notification.generate_identifier },
+          Notification.update_all( { identifier: notification.generate_identifier },
                                    id: notification.id )
       end
   end
