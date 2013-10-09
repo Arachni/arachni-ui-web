e148b8bcd321e80359dd6f1f2121bbfb8ed1681a
Tasos Laskos <tasos.laskos@gmail.com>
2013-10-09 22:25:17 +0300

    AddScheduleIdToScans migration: Assign Schedules to old scans

--- db/migrate/20131003154327_add_schedule_id_to_scans.rb
+++ db/migrate/20131003154327_add_schedule_id_to_scans.rb
@@ -1,5 +1,12 @@
 class AddScheduleIdToScans < ActiveRecord::Migration
-  def change
-    add_column :scans, :schedule_id, :integer
-  end
+    def change
+        add_column :scans, :schedule_id, :integer
+
+        Scan.all.each do |scan|
+            next if scan.schedule
+
+            schedule_id = scan.root ? scan.root.schedule_id : Schedule.create( basetime: :started_at ).id
+            Scan.update_all( { schedule_id: schedule_id }, { id: scan.id } )
+        end
+    end
 end
