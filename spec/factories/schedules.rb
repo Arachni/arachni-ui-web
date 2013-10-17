10654639ac965428565de915c6e4a20399972d3d
Tasos Laskos <tasos.laskos@gmail.com>
2013-10-17 21:55:25 +0300

    Added Scheduler spec placeholders

---
+++ spec/factories/schedules.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :schedule do
+    type ""
+    datetime "2013-09-27 23:02:57"
+    scan_id 1
+  end
+end
