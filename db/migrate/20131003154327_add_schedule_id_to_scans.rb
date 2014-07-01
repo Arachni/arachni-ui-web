e11b5cfb8de45c6329a359cf12c282364fc5078d
Tasos Laskos <tasos.laskos@gmail.com>
2014-07-01 04:42:42 +0300

    db/migrate: Check for nil #find_each values

--- db/migrate/20131003154327_add_schedule_id_to_scans.rb
+++ db/migrate/20131003154327_add_schedule_id_to_scans.rb
@@ -2,7 +2,7 @@ class AddScheduleIdToScans < ActiveRecord::Migration
     def change
         add_column :scans, :schedule_id, :integer
 
-        Scan.find_each.each do |scan|
+        (Scan.find_each || []).each do |scan|
             next if scan.schedule
 
             schedule_id = scan.root ? scan.root.schedule_id : Schedule.create( basetime: :started_at ).id
--- db/migrate/20140629003131_update_issues_for_frameworkv05.rb
+++ db/migrate/20140629003131_update_issues_for_frameworkv05.rb
@@ -7,7 +7,7 @@ class UpdateIssuesForFrameworkv05 < ActiveRecord::Migration
         add_column :issues, :response,      :text
         add_column :issues, :request,       :text
 
-        Issue.find_each.each do |issue|
+        (Issue.find_each || []).each do |issue|
             inputs = issue.schedule.audit_options[:params]
             next if !inputs
 
