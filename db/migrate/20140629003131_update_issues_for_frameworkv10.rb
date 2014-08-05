17c42389364c345048eca5eda53bd5056b81d00c
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-05 12:54:51 +0300

    UpdateIssuesForFrameworkv10 migration: Updated vector input migration

--- db/migrate/20140629003131_update_issues_for_frameworkv10.rb
+++ db/migrate/20140629003131_update_issues_for_frameworkv10.rb
@@ -1,7 +1,6 @@
 class UpdateIssuesForFrameworkv10 < ActiveRecord::Migration
     def change
         remove_column :issues, :headers
-        remove_column :issues, :audit_options
 
         add_column :issues, :vector_inputs,   :text
         add_column :issues, :vector_html,     :text
@@ -11,10 +10,20 @@ class UpdateIssuesForFrameworkv10 < ActiveRecord::Migration
         add_column :issues, :request,         :text
 
         (Issue.find_each || []).each do |issue|
-            inputs = issue.schedule.audit_options[:params]
+            sql = "SELECT * from issues WHERE \"id\"=#{issue.id}"
+            result = ActiveRecord::Base.connection.execute(sql)
+
+            audit_options = result.first['audit_options']
+            next if !audit_options
+
+            audit_options = YAML.load( audit_options )
+
+            inputs = audit_options['params']
             next if !inputs
 
-            Issue.update_all( { vector_inputs: inputs }, { id: issue.id } )
+            Issue.where( id: issue.id ).update_all( vector_inputs: inputs )
         end
+
+        remove_column :issues, :audit_options
     end
 end
