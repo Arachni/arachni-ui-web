00e1e8ac9eed8b96628a6b7ef163db80948ead8e
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-28 05:32:49 +0300

    ApplicationHelper#m: Force encoding to binary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,8 +56,8 @@ module ApplicationHelper
     end
 
     def m( string )
-        html = Kramdown::Document.new( string ).to_html.recode
-        Loofah.fragment( html ).scrub!(:prune).to_s.html_safe.force_encoding( 'binary')
+        html = Kramdown::Document.new( string.recode ).to_html
+        Loofah.fragment( html ).scrub!(:prune).to_s.html_safe
     end
 
     def truncate_html( html, length = 500, append = ' [...]' )
