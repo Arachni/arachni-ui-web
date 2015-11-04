d83a35c8656c6b779bd2056f343283f98c9b5cdc
Tasos Laskos <tasos.laskos@gmail.com>
2015-11-05 01:22:03 +0200

    config/environments/production: Set options to silence warnings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ ArachniWebui::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   # Causes issues on Windows the precompilling assets so disable it.
@@ -43,7 +43,7 @@ ArachniWebui::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
